364cb624c460e1bd13f31d9ba4d4c23f9de76211
AarySlayz <aaryusagar12@gmail.com>
2025-07-21 00:12:37 -0400

    Initial Commit

---
+++ src/main/java/net/aaryslayz/seanluckymod/SeanLuckyModClient.java
@@ -0,0 +1,10 @@
+package net.aaryslayz.seanluckymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SeanLuckyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aaryslayz/seanluckymod/SeanLuckyModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aaryslayz.seanluckymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SeanLuckyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
